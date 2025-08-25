30b73195686e7afe10de2f35b063cdd205de94cd
Vlajs <149913552+Vlajs@users.noreply.github.com>
2025-08-25 16:46:52 +0200

    Update Nizovi.java

--- src/Nizovi.java
+++ src/Nizovi.java
@@ -4,7 +4,8 @@ public class Nizovi {
 
      niz[0] = 5;
      niz[1] = 6;
-     niz[2] = 7;
+
+     niz[2] = 9;
 
      /*for (int i = 0; i < niz.length; i++){
        System.out.println(niz[i]);
@@ -34,6 +35,7 @@ public class Nizovi {
               System.out.print(b + " ");
           }
           System.out.println("\n");
+        System.out.println("Ovo je kraj koda");
       }
 
 
